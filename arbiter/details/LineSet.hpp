649cb9dafbb29bb27c45ba4ec4b2675cbaa12967
Austin Gilbert <ceretullis@gmail.com>
2015-05-06 17:02:55 -0500

    Adding LineSet abrstraction to help with reporting duplicates on a given line, will migrate SequenceCount to using this rather than a simple integer count of occurrances.

--- arbiter/details/LineSet.hpp
+++ arbiter/details/LineSet.hpp
@@ -1,7 +1,5 @@
 #pragma once 
 #include <bitset>
-#include <cstddef>
-#include <vector>
 
 namespace arbiter { namespace details {
 
@@ -11,16 +9,13 @@ namespace arbiter { namespace details {
     class LineSet
     {
     public:
+        // returns false if lineId is already in the set.
+        bool insert(const std::size_t lineId);
 
-        bool insert(const std::size_t lineId);  // false if lineId in set already
-        bool complete() const;   // true if all lines in set
-        bool empty() const;      // true if no lines are in set
+        // returns true if there are lines missing.
+        bool missing() const;
 
         bool operator[](const std::size_t index);
-
-        std::vector<std::size_t> missing() const;
-
-        void fill();
 
     private:
         std::bitset<NumberOfLines> value_;
@@ -37,43 +32,14 @@ namespace arbiter { namespace details {
     }
 
     template<std::size_t NumberOfLines>
-    bool LineSet<NumberOfLines>::complete() const
+    bool LineSet<NumberOfLines>::missing() const
     {
-        return value_.count() == NumberOfLines;
-    }
-
-    template<std::size_t NumberOfLines>
-    bool LineSet<NumberOfLines>::empty() const
-    {
-        return value_.count() == 0;
+        return value_.count() != NumberOfLines;
     }
 
     template<std::size_t NumberOfLines>
     bool LineSet<NumberOfLines>::operator[](const std::size_t index)
     {
         return value_[index];
-    }
-
-    template<std::size_t NumberOfLines>
-    std::vector<std::size_t> LineSet<NumberOfLines>::missing() const
-    {
-        std::vector<std::size_t> missingLines;
-        missingLines.reserve(NumberOfLines);
-
-        for(std::size_t i = 0, end = value_.size(); i < end; ++i)
-        {
-            if(!value_[i])
-            {
-                missingLines.emplace_back(i);
-            }
-        }
-
-        return missingLines;
-    }
-
-    template<std::size_t NumberOfLines>
-    void LineSet<NumberOfLines>::fill()
-    {
-        value_.set();
     }
 }}
--- arbiter/tests/unit_test/testLineSet-UT.cpp
+++ arbiter/tests/unit_test/testLineSet-UT.cpp
@@ -1,6 +1,5 @@
 #include "./platform/UnitTestSupport.hpp"
 #include <arbiter/details/LineSet.hpp>
-#include <stdexcept>
 
 namespace {
 
@@ -39,58 +38,5 @@ namespace {
 
         CHECK(set[0]);
         CHECK(set[1]);
-    }
-
-    TEST(verifyEmpty)
-    {
-        arbiter::details::LineSet<2> set;
-        CHECK(set.empty());
-        set.insert(0);
-        CHECK(!set.empty());
-    }
-
-    TEST(verifyComplete)
-    {
-        arbiter::details::LineSet<2> set;
-        CHECK(!set.complete());
-
-        set.insert(0);
-        CHECK(!set.complete());
-
-        set.insert(1);
-        CHECK(set.complete());
-    }
-
-    TEST(verifyMissingLines)
-    {
-        arbiter::details::LineSet<3> set;
-        auto missing = set.missing();
-
-        /*REQUIRE*/ CHECK_EQUAL(3U, missing.size());
-        CHECK_EQUAL(0U, missing[0]);
-        CHECK_EQUAL(1U, missing[1]);
-        CHECK_EQUAL(2U, missing[2]);
-
-        set.insert(1);
-        missing = set.missing();
-
-        /*REQUIRE*/ CHECK_EQUAL(2U, missing.size());
-        CHECK_EQUAL(0U, missing[0]);
-        CHECK_EQUAL(2U, missing[1]);
-    }
-
-    TEST(verifyThrowsOnOutOfRange)
-    {
-        arbiter::details::LineSet<2> set;
-        CHECK_THROW(set.insert(4), std::out_of_range);
-    }
-
-    TEST(verifyFill)
-    {
-        arbiter::details::LineSet<3> set;
-        CHECK(!set.complete());
-
-        set.fill();
-        CHECK(set.complete());
     }
 }
