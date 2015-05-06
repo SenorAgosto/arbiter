64c4946f059adb6e076c6e8793285dd5fbef471c
Austin Gilbert <ceretullis@gmail.com>
2015-05-06 12:08:25 -0500

    Eliminating unused parameter warnings.

--- arbiter/details/NullErrorReportingPolicy.hpp
+++ arbiter/details/NullErrorReportingPolicy.hpp
@@ -9,7 +9,7 @@ namespace arbiter { namespace details {
 		void DuplicateOnLine(const std::size_t /*line*/, const SequenceType /*sequence*/){}
 		
 		void Gap(const SequenceType /*start*/, const SequenceType /*length*/){}
-		void GapFill(const SequenceType start, const SequenceType /*length*/){}
+		void GapFill(const SequenceType /*start*/, const SequenceType /*length*/){}
 		
 		void LinePositionOverrun(const std::size_t /*slowLine*/, const std::size_t /*overrunByLine*/){}
 		void UnrecoverableGap(const std::size_t /*line*/, const SequenceType /*start*/, const SequenceType /*length*/){}	// reported if a sequence we're replacing has count of 0.
