68e58af6f62fd490c1ae47be15fca54d9fe28b3f
Austin Gilbert <ceretullis@gmail.com>
2015-05-08 12:23:17 -0500

    Adding a construct that supplies the lineId in addition to the Sequence number.

--- arbiter/tests/unit_test/testSequenceInfo-UT.cpp
+++ arbiter/tests/unit_test/testSequenceInfo-UT.cpp
@@ -3,9 +3,17 @@
 
 namespace {
 
+    TEST(verifyInstantiationOfSequenceInfoWithSingleLineIsComplete)
+    {
+        arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo(0, 0);
+
+        CHECK(sequenceInfo.complete());
+        CHECK_EQUAL(0U, sequenceInfo.sequence());
+    }
+
 	TEST(verifyInstantiationOfSequenceInfo)
 	{
-		arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo(0);
+		arbiter::details::SequenceInfo<std::size_t, 3> sequenceInfo(0, 0);
 		
 		CHECK(!sequenceInfo.complete());
         CHECK_EQUAL(0U, sequenceInfo.sequence());
