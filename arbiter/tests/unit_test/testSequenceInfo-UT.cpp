19b200c8457b3acd4416cc8803a88b83e45a017b
Austin Gilbert <ceretullis@gmail.com>
2015-05-08 11:40:10 -0500

    testSequenceCount-UT.cpp -> testSequenceInfo-UT.cpp

--- arbiter/tests/unit_test/testSequenceInfo-UT.cpp
+++ arbiter/tests/unit_test/testSequenceInfo-UT.cpp
@@ -3,17 +3,9 @@
 
 namespace {
 
-    TEST(verifyInstantiationOfSequenceInfoWithSingleLineIsComplete)
-    {
-        arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo(0, 0);
-
-        CHECK(sequenceInfo.complete());
-        CHECK_EQUAL(0U, sequenceInfo.sequence());
-    }
-
 	TEST(verifyInstantiationOfSequenceInfo)
 	{
-		arbiter::details::SequenceInfo<std::size_t, 3> sequenceInfo(0, 0);
+		arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo(0);
 		
 		CHECK(!sequenceInfo.complete());
         CHECK_EQUAL(0U, sequenceInfo.sequence());
@@ -25,14 +17,5 @@ namespace {
 
         CHECK(sequenceInfo.complete());
         CHECK_EQUAL(0U, sequenceInfo.sequence());
-    }
-
-    TEST(verifyGapReported)
-    {
-        // created a sequenceInfo with sequence 0, no lines reporting means this is a gap.
-        arbiter::details::SequenceInfo<std::size_t, 2> sequenceInfo(0);
-
-        CHECK(!sequenceInfo.complete());
-        CHECK(sequenceInfo.empty());
     }
 }
