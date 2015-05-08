3dce7a6f2ec46751c8e2a0431cba71600e3f81e6
Austin Gilbert <ceretullis@gmail.com>
2015-05-08 11:39:34 -0500

    Eliminated invalid() replaced with different semantics using complete().

--- arbiter/details/SequenceInfo.hpp
+++ arbiter/details/SequenceInfo.hpp
@@ -1,93 +1,61 @@
 #pragma once 
+#include <arbiter/details/LineSet.hpp>
 #include <cstddef>
 
 namespace arbiter { namespace details {
 
 	// A structure to hold a count of sequence numbers
-	template<typename SequenceType>
+	template<typename SequenceType, std::size_t NumberOfLines>
 	class SequenceInfo 
 	{
 	public:
+        using LineSet = details::LineSet<NumberOfLines>;
+
 		SequenceInfo();
 		SequenceInfo(const SequenceType sequence);
-		
-		inline std::size_t operator++();		// prefix increment 
-		inline std::size_t operator++(int);   	// postfix increment 
-		
+
+        inline void insert(const std::size_t lineId);
+        inline bool complete() const;
+
+        inline const LineSet& lines() const;
+
 		inline SequenceType sequence() const;
 		inline void sequence(const SequenceType seq);
 
-		inline std::size_t count() const;
-		inline void count(const std::size_t value);
-
-		inline void invalidate();
-		inline bool invalid() const;
-
 	private:
-		std::size_t count_;
+        LineSet lines_;
 		SequenceType sequence_;
 	};
 	
 
-	template<typename SequenceType>
-	SequenceInfo<SequenceType>::SequenceInfo()
-		: count_(std::numeric_limits<std::size_t>::max())
-		, sequence_()
+	template<typename SequenceType, std::size_t NumberOfLines>
+	SequenceInfo<SequenceType, NumberOfLines>::SequenceInfo()
+        : sequence_()
 	{
+        lines_.fill();
 	}
 	
-	template<typename SequenceType>
-	SequenceInfo<SequenceType>::SequenceInfo(const SequenceType sequence)
-		: count_(1)
-		, sequence_(sequence)
+	template<typename SequenceType, std::size_t NumberOfLines>
+	SequenceInfo<SequenceType, NumberOfLines>::SequenceInfo(const SequenceType sequence)
+		: sequence_(sequence)
 	{
 	}
 
-	template<typename SequenceType>
-	std::size_t SequenceInfo<SequenceType>::operator++()
-	{
-		return ++count_;
-	}
-	
-	template<typename SequenceType>
-	std::size_t SequenceInfo<SequenceType>::operator++(int)
-	{
-		return count_++;
-	}
-	
-	template<typename SequenceType>
-	SequenceType SequenceInfo<SequenceType>::sequence() const 
+	template<typename SequenceType, std::size_t NumberOfLines>
+	SequenceType SequenceInfo<SequenceType, NumberOfLines>::sequence() const
 	{
 		return sequence_;
 	}
 	
-	template<typename SequenceType>
-	void SequenceInfo<SequenceType>::sequence(const SequenceType seq)
+	template<typename SequenceType, std::size_t NumberOfLines>
+	void SequenceInfo<SequenceType, NumberOfLines>::sequence(const SequenceType seq)
 	{
 		sequence_ = seq;
 	}
 
-	template<typename SequenceType>
-	std::size_t SequenceInfo<SequenceType>::count() const 
-	{
-		return count_;
-	}
-
-	template<typename SequenceType>
-	void SequenceInfo<SequenceType>::count(const std::size_t value) 
-	{
-		count_ = value;
-	}
-	
-	template<typename SequenceType>
-	void SequenceInfo<SequenceType>::invalidate()
-	{
-		count_ = std::numeric_limits<std::size_t>::max();
-	}
-
-	template<typename SequenceType>
-	bool SequenceInfo<SequenceType>::invalid() const 
-	{
-		return count_ == std::numeric_limits<std::size_t>::max();
-	}	
+    template<typename SequenceType, std::size_t NumberOfLines>
+    bool SequenceInfo<SequenceType, NumberOfLines>::complete() const
+    {
+        return lines_.complete();
+    }
 }}
