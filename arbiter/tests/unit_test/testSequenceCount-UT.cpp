67d0d77f21b3be1e6d194d81d928eda55ebd99e7
Austin Gilbert <ceretullis@gmail.com>
2015-05-08 11:39:34 -0500

    Eliminated invalid() replaced with different semantics using complete().

--- arbiter/tests/unit_test/testSequenceCount-UT.cpp
+++ arbiter/tests/unit_test/testSequenceCount-UT.cpp
@@ -5,59 +5,17 @@ namespace {
 
 	TEST(verifyInstantiationOfSequenceInfo)
 	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo(0);
+		arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo(0);
 		
-		CHECK_EQUAL(1U, sequenceInfo.count());
-		CHECK_EQUAL(0U, sequenceInfo.sequence());
-	}
-	
-	TEST(verifyInstantiationWithDefaultConstructor)
-	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo;
-		CHECK(sequenceInfo.invalid());
-	}
-	
-	TEST(verifyInvalidate)
-	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo(0);
-		
-		CHECK(!sequenceInfo.invalid());
-		sequenceInfo.invalidate();
-		CHECK(sequenceInfo.invalid());
+		CHECK(!sequenceInfo.complete());
+        CHECK_EQUAL(0U, sequenceInfo.sequence());
 	}
 
-	TEST(verifyPrefixIncrement)
-	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo(0);
-		
-		CHECK_EQUAL(1U, sequenceInfo.count());
-		auto s = ++sequenceInfo;
-		
-		CHECK_EQUAL(2U, s);
-		CHECK_EQUAL(2U, sequenceInfo.count());
-	}
-	
-	TEST(verifyPostfixIncrement)
-	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo(0);
-		
-		CHECK_EQUAL(1U, sequenceInfo.count());
-		auto s = sequenceInfo++;
-		
-		CHECK_EQUAL(1U, s);
-		CHECK_EQUAL(2U, sequenceInfo.count());
-	}
+    TEST(verifyInstantiationOfSequenceInfoWithDefaultConstructor)
+    {
+        arbiter::details::SequenceInfo<std::size_t, 1> sequenceInfo;
 
-	TEST(verifySetters)
-	{
-		arbiter::details::SequenceInfo<std::size_t> sequenceInfo;
-		CHECK(sequenceInfo.invalid());
-		
-		sequenceInfo.count(100);
-		sequenceInfo.sequence(15);
-
-		CHECK(!sequenceInfo.invalid());
-		CHECK_EQUAL(100U, sequenceInfo.count());
-		CHECK_EQUAL(15U, sequenceInfo.sequence());
-	}
+        CHECK(sequenceInfo.complete());
+        CHECK_EQUAL(0U, sequenceInfo.sequence());
+    }
 }
