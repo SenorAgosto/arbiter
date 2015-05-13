b7aeb37593a74a89c7d791895ea58e6e998a7d3a
Austin Gilbert <ceretullis@gmail.com>
2015-05-13 17:39:26 -0500

    An object instantiating all arbiter cache advancer states and implementing a operator[] for selecting the correct state instance based on an enum.

---
+++ arbiter/details/states/ArbiterStatesPack.hpp
@@ -0,0 +1,55 @@
+#pragma once 
+
+#include <arbiter/details/states/InitialState.hpp>
+#include <arbiter/details/states/AdvanceHead.hpp>
+#include <arbiter/details/states/AdvanceLine.hpp>
+#include <arbiter/details/states/GapFill.hpp>
+#include <arbiter/details/states/HeadForwardGapFill.hpp>
+#include <arbiter/details/states/LineForwardGapFill.hpp>
+
+#include <arbiter/Exceptions.hpp>
+
+namespace arbiter { namespace details {
+
+    // Instantiate all the ArbiterCacheAdvancerStates and
+    // overload the operator[] for getting a reference to them.
+    template<class Traits>
+    class ArbiterStatesPack
+    {
+    public:
+        ArbiterCacheAdvancerState<Traits>& operator[](ArbiterCacheAdvancerStateEnum state);
+
+    private:
+        InitialState<Traits> initialState_;
+        AdvanceHead<Traits> advanceHead_;
+        AdvanceLine<Traits> advanceLine_;
+        GapFill<Traits> gapFill_;
+        HeadForwardGapFill<Traits> headForwardGapFill_;
+        LineForwardGapFill<Traits> lineForwardGapFill_;
+    };
+
+    template<class Traits>
+    ArbiterCacheAdvancerState<Traits>& ArbiterStatesPack<Traits>::operator[](ArbiterCacheAdvancerStateEnum state)
+    {
+        // order by frequency of occurance...
+        switch(state)
+        {
+        case ArbiterCacheAdvancerStateEnum::AdvanceHead:
+            return advanceHead_;
+        case ArbiterCacheAdvancerStateEnum::AdvanceLine:
+            return advanceLine_;
+        case ArbiterCacheAdvancerStateEnum::GapFill:
+            return gapFill_;
+        case ArbiterCacheAdvancerStateEnum::HeadForwardGapFill:
+            return headForwardGapFill_;
+        case ArbiterCacheAdvancerStateEnum::LineForwardGapFill:
+            return lineForwardGapFill_;
+        case ArbiterCacheAdvancerStateEnum::InitialState:
+            return initialState_;
+        case ArbiterCacheAdvancerStateEnum::NumberOfEntries:
+            break;  // fall through
+        };
+
+        throw ArbiterCacheAdvancerStateEnumOutOfRange(static_cast<std::size_t>(state));
+    }
+}}
