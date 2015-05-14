6d37b3737525ab30ff2c4c851f9cc1d803047565
Austin Gilbert <ceretullis@gmail.com>
2015-05-14 15:08:44 -0500

    Initial implementation of AdvanceLine.

--- arbiter/details/states/AdvanceLine.hpp
+++ arbiter/details/states/AdvanceLine.hpp
@@ -15,6 +15,14 @@ namespace arbiter { namespace details {
     template<class Traits>
     bool AdvanceLine<Traits>::advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber)
     {
+        auto& cache = context.cache;
+
+        auto nextPosition = cache.nextPosition(lineId);
+        auto& sequenceInfo = cache.history[nextPosition];
+
+        sequenceInfo.insert(lineId);
+        cache.positions[lineId] = nextPosition;
+
         return false;   // we've seen the sequence number before...
     }
 }}
