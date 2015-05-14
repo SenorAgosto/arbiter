8052a9db3bfa897b493e92518184a0d373525e24
Austin Gilbert <ceretullis@gmail.com>
2015-05-13 20:56:03 -0500

    Initial implementation of AdvanceHead.     TODO: doesn't handle slow-line overrun yet.

--- arbiter/details/states/AdvanceHead.hpp
+++ arbiter/details/states/AdvanceHead.hpp
@@ -7,14 +7,51 @@ namespace arbiter { namespace details {
     class AdvanceHead : public ArbiterCacheAdvancerState<Traits>
     {
     public:
+        using ErrorReportingPolicy = typename Traits::ErrorReportingPolicy;
         using SequenceType = typename Traits::SequenceType;
+        using SequenceInfo = SequenceInfo<SequenceType, Traits::NumberOfLines()>;
 
         bool advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber) override;
+
+    private:
+        void handleGaps(const SequenceInfo& sequenceInfo, ErrorReportingPolicy& errorPolicy);
     };
+
 
     template<class Traits>
     bool AdvanceHead<Traits>::advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber)
     {
-        return true;    // new sequence number
+        auto& cache = context.cache;
+
+        auto nextPosition = cache.nextPosition(lineId);
+        auto& sequenceInfo = cache.history[nextPosition];
+
+        // TODO: check for slow line overrun...
+        handleGaps(sequenceInfo, context.errorPolicy);
+
+        cache.history[nextPosition] = SequenceInfo(lineId, sequenceNumber);
+        cache.positions[lineId] = nextPosition;
+
+        return true;    // new sequence number, accept the message
+    }
+
+    template<class Traits>
+    void AdvanceHead<Traits>::handleGaps(const SequenceInfo& sequenceInfo, ErrorReportingPolicy& errorPolicy)
+    {
+        if(!sequenceInfo.complete())
+        {
+            if(sequenceInfo.empty())
+            {
+                errorPolicy.UnrecoverableGap(sequenceInfo.sequence());
+            }
+            else
+            {
+                const auto& missingLines = sequenceInfo.lines().missing();
+                for(auto line : missingLines)
+                {
+                    errorPolicy.UnrecoverableLineGap(line, sequenceInfo.sequence());
+                }
+            }
+        }
     }
 }}
