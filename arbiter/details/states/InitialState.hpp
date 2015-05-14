7dce6cbcac0ca0a9f3d69436ed0dcbed2a362d40
Austin Gilbert <ceretullis@gmail.com>
2015-05-13 20:31:37 -0500

    Initial implementation of InitialState.     TODO: Doesn't handle gaps on first messages yet.

--- arbiter/details/states/InitialState.hpp
+++ arbiter/details/states/InitialState.hpp
@@ -8,13 +8,38 @@ namespace arbiter { namespace details {
     {
     public:
         using SequenceType = typename Traits::SequenceType;
-        
+        using SequenceInfo = SequenceInfo<SequenceType, Traits::NumberOfLines()>;
+
         bool advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber) override;
     };
 
     template<class Traits>
     bool InitialState<Traits>::advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber)
     {
-        return true;    // first sequence number is always new...
+        std::size_t position = 0;
+
+        if(sequenceNumber != Traits::FirstExpectedSequenceNumber())
+        {
+            if(sequenceNumber < Traits::FirstExpectedSequenceNumber())
+            {
+                // report out of sequence
+                return false;
+            }
+            else
+            {
+                // report the gap...
+                //context.errorPolicy.Gap(0, sequenceNumber - 1);
+
+                // for the current position to sequenceNumber, iterate through
+                // the cache creating the correct gap SequenceInfos
+            }
+        }
+
+        context.cache.history[position] = SequenceInfo(lineId, sequenceNumber);
+
+        context.cache.positions[lineId] = context.cache.nextPosition(lineId);
+        context.cache.head = lineId;
+
+        return true;
     }
 }}
