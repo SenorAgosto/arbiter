18b99ac9316931453a0dc89560e121468ab9430d
Austin Gilbert <ceretullis@gmail.com>
2015-05-18 17:27:24 -0500

    Fleshing out unit test coverage for various forms of gaps, making code corrections as needed.

--- arbiter/details/states/AdvanceLine.hpp
+++ arbiter/details/states/AdvanceLine.hpp
@@ -12,6 +12,7 @@ namespace arbiter { namespace details {
         bool advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber) override;
     };
 
+
     template<class Traits>
     bool AdvanceLine<Traits>::advance(ArbiterCacheAdvancerContext<Traits>& context, const std::size_t lineId, const SequenceType sequenceNumber)
     {
@@ -20,9 +21,21 @@ namespace arbiter { namespace details {
         auto nextPosition = cache.nextPosition(lineId);
         auto& sequenceInfo = cache.history[nextPosition];
 
+        bool sequenceMatch = sequenceNumber == sequenceInfo.sequence();
+        bool accept = sequenceMatch && sequenceInfo.empty();
+
+        if(accept)
+        {
+            context.errorPolicy.GapFill(sequenceNumber, 1);
+        }
+        else if(sequenceMatch && sequenceInfo.has(lineId))
+        {
+            context.errorPolicy.DuplicateOnLine(lineId, sequenceNumber);
+        }
+
         sequenceInfo.insert(lineId);
         cache.positions[lineId] = nextPosition;
 
-        return false;   // we've seen the sequence number before...
+        return accept;
     }
 }}
