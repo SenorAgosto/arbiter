4589095c3e6db1bbdab0677eecfc1056dbbe2010
Austin Gilbert <ceretullis@gmail.com>
2016-02-24 17:07:45 -0600

    Adding a CMake flag -DANALYSIS=1 to disable some warning settings. The crux of the problem is GCC happily uses the -Wno-* flags, but scan-build uses clang which produces errors when the flags are encountered. So, in analysis mode, we won't turn all warnings on.

--- _cmake/compilers/gcc49.cmake
+++ _cmake/compilers/gcc49.cmake
@@ -1,6 +1,10 @@
 add_definitions(
 	    --std=c++11
+)
 
+# ANALYSIS will be defined when using scan-build
+if(NOT DEFINED ANALYSIS)
+    add_definitions(
             -Wall
             -Wextra 
 
@@ -9,15 +13,16 @@ add_definitions(
             -Wno-unused-variable
         )
 
-if(${CMAKE_BUILD_TYPE} MATCHES "Debug") 
-    add_definitions(
-	-g
-	-O0
-    )
-endif() 
+    if(${CMAKE_BUILD_TYPE} MATCHES "Debug") 
+        add_definitions(
+	    -g
+	    -O0
+        )
+    endif() 
 
-if(${CMAKE_BUILD_TYPE} MATCHES "Release")
-    add_definitions(
-	-O4
-    )
+    if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+        add_definitions(
+	    -O4
+        )
+    endif()
 endif()
